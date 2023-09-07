a9d3ea5028927f380e40a31db01980445bf43642
Juanda Esa Putra <C010221002@bankmandiri.co.id>
2023-09-07 17:15:39 +0700

    fixed theme mode

--- src/components/themeswitch/ThemeToggle.tsx
+++ src/components/themeswitch/ThemeToggle.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 
 const ThemeToggle = () => {
-  const [mounted, setMounted] = useState(false)
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme, resolvedTheme } = useTheme();
 
   useEffect(() => setMounted(true), []);
@@ -13,10 +13,11 @@ const ThemeToggle = () => {
       <input
         type="checkbox"
         checked={mounted && (theme === "dark" || resolvedTheme === "dark")}
-        onChange={() => 
-        setTheme(
-          theme === "dark" || resolvedTheme === "dark" ? "light" : "dark"
-        )}
+        onChange={() =>
+          setTheme(
+            theme === "dark" || resolvedTheme === "dark" ? "light" : "dark"
+          )
+        }
       />
       <span className="slider"></span>
     </label>
