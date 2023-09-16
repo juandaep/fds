e3b2a8040e1aac22c44396ad5b62f0cb650fbb3a
juandaep <36181732+juandaep@users.noreply.github.com>
2023-09-17 03:48:45 +0700

    framer motion

--- src/components/sidebar/MobileSidebar.tsx
+++ src/components/sidebar/MobileSidebar.tsx
@@ -1,28 +1,42 @@
-import React, { FC, useEffect } from 'react'
+import { AnimatePresence, easeIn, motion } from "framer-motion";
+import React, { FC, useEffect } from "react";
+import { ComponentsNavbar } from "../navbar/ComponentsNavbar";
 
 type SidebarProps = {
-    isOpen: boolean
-    onClose: () => void
-    children: React.ReactNode
-}
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+};
 
-export const MobileSidebar:FC<SidebarProps> = ({isOpen, onClose, children}) => {
-    useEffect(() => {
-        if (isOpen) {
-            document.body.style.overflow = "hidden"
-        } else {
-            document.body.style.overflow = "auto"
-        }
-    }, [isOpen, onClose])
+export const MobileSidebar: FC<SidebarProps> = ({
+  isOpen,
+  onClose,
+  children,
+}) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isOpen, onClose]);
   return (
-    <>
-    {isOpen && (
-        <div className='fixed inset-0 top-24 flex min-h-screen items-center justify-center z-40'>
-            <div className='fixed w-full h-full bg-white'>
-                {children}
-            </div>
-        </div>
-    )}
-    </>
-  )
-}
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ x: "-100%" }}
+          animate={{
+            x: 0,
+          }}
+          exit={{
+            x: "-100%",
+          }}
+          transition={{ type: "spring", bounce: 0, duration: 0.4 }}
+          className="fixed inset-0 top-24 flex min-h-screen items-center justify-center z-40"
+        >
+          <div className="fixed w-full h-full bg-white">{children}</div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
